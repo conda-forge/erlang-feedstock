d01df86edf9cb227e030bd86cd16ba23efb7b414
jakirkham <jakirkham@gmail.com>
2016-08-09 07:25:05 -0400

    recipe: Comment the rest of the tests.

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,7 +7,7 @@ export ERL_TOP="$(pwd)"
   --with-libatomic_ops="${PREFIX}" --enable-m${ARCH}-build
 make
 #make release_tests
-cd "${ERL_TOP}/release/tests/test_server"
-${ERL_TOP}/bin/erl -s ts install -s ts smoke_test batch -s init stop
+#cd "${ERL_TOP}/release/tests/test_server"
+#${ERL_TOP}/bin/erl -s ts install -s ts smoke_test batch -s init stop
 cd ${ERL_TOP}
 make install
